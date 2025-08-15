286184e37e8c137047f81e5358c788a8db31d060
basic-bitch-foundation <ytpartnership2427@gmail.com>
2025-08-15 18:29:45 +0530

    first commit

---
+++ player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@export var spd := 400       
+@export var jmp_forcrc := -300  
+@export var gravity := 600   
+
+func _physics_process(delta: float) -> void:
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		velocity.y = 0  
+
+	
+	var input_direction = Input.get_axis("ui_left", "ui_right")
+	velocity.x = input_direction * spd
+
+	
+	if Input.is_action_just_pressed("ui_jump") and is_on_floor():
+		velocity.y = jmp_forcrc
+
+	
+	move_and_slide()
