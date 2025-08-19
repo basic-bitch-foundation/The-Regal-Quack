2924618fdf091e6cf0b83dd667fe3e4ab17bc5fc
basic-bitch-foundation <ytpartnership2427@gmail.com>
2025-08-19 09:23:32 +0530

    final edit
    _add king statue for tribute aand win consition
    -Add win text aand message for three sen then resapwan

--- player.gd
+++ player.gd
@@ -12,11 +12,15 @@ var spawnpoint: Marker2D
 var collectlayer: TileMapLayer = null
 var isdead: bool = false
 var deathtimer: float = 0.0
+var iswon: bool = false
+var wintimer: float = 0.0
 
 @onready var skull1: Sprite2D
 @onready var skull2: Sprite2D  
 @onready var skull3: Sprite2D
 @onready var animsprite: AnimatedSprite2D
+@onready var winpanel: Panel
+@onready var wintext: RichTextLabel
 
 @onready var bgmusic: AudioStreamPlayer
 @onready var jumpsfx: AudioStreamPlayer
@@ -25,6 +29,7 @@ var deathtimer: float = 0.0
 @onready var chickensfx: AudioStreamPlayer
 @onready var deathsfx: AudioStreamPlayer
 @onready var restartsfx: AudioStreamPlayer
+@onready var winsfx: AudioStreamPlayer
 
 func _ready() -> void:
 	add_to_group("player")
@@ -37,6 +42,13 @@ func _ready() -> void:
 	skull2 = $UI/Skull2
 	skull3 = $UI/Skull3
 	animsprite = $AnimatedSprite2D
+	winpanel = get_tree().current_scene.find_child("WinPanel", true, false)
+	wintext = get_tree().current_scene.find_child("WinText", true, false)
+	
+	if not winpanel:
+		print("ERROR: WinPanel not found at CanvasLayer/UIKING/WinPanel")
+	if not wintext:
+		print("ERROR: WinText not found at CanvasLayer/UIKING/WinPanel/WinText")
 	
 	bgmusic = $SFX/BGMusic
 	jumpsfx = $SFX/JumpSFX
@@ -45,12 +57,29 @@ func _ready() -> void:
 	chickensfx = $SFX/ChickenSFX
 	deathsfx = $SFX/DeathSFX
 	restartsfx = $SFX/RestartSFX
+	winsfx = $SFX/WinSFX
 	
 	if bgmusic:
 		bgmusic.play()
 	
+	if winpanel:
+		winpanel.visible = false
+	
 	updateskulls()
 	findlayer()
+	connectking()
+
+func connectking() -> void:
+	var kingbody = get_tree().current_scene.find_child("KingBody", true, false)
+	if kingbody:
+		var kingarea = kingbody.find_child("Area2D", true, false)
+		if kingarea:
+			kingarea.body_entered.connect(_on_king_touched)
+
+func _on_king_touched(body: Node2D) -> void:
+	if body == self:
+		print("King touched! Calling win()")
+		win()
 
 func updateskulls() -> void:
 	skull1.visible = (hp >= 1)
@@ -69,6 +98,12 @@ func findlayer() -> void:
 		collectlayer = get_parent().get_node_or_null("TileMap2/Layer1")
 
 func _physics_process(delta: float) -> void:
+	if iswon:
+		wintimer -= delta
+		if wintimer <= 0.0:
+			dorespawn()
+		return
+		
 	if isdead:
 		deathtimer -= delta
 		if deathtimer <= 0.0:
@@ -120,7 +155,9 @@ func updateanims(inputdir: float) -> void:
 	elif inputdir < 0:
 		animsprite.flip_h = true
 	
-	if isdead:
+	if iswon:
+		animsprite.play("idle")
+	elif isdead:
 		animsprite.play("death")
 	elif touchingenemy:
 		animsprite.play("hurt")
@@ -132,7 +169,7 @@ func updateanims(inputdir: float) -> void:
 		animsprite.play("idle")
 
 func hurt(amount: int) -> void:
-	if isdead:
+	if isdead or iswon:
 		return
 	
 	bghp -= amount
@@ -162,20 +199,46 @@ func dorespawn() -> void:
 	hp = maxhp
 	bghp = float(maxhp)
 	isdead = false
+	iswon = false
 	deathtimer = 0.0
+	wintimer = 0.0
 	touchingenemy = false
 	velocity = Vector2.ZERO
 	updateskulls()
+	
+	if winpanel:
+		winpanel.visible = false
 
 func start_damage() -> void:
-	if not isdead:
+	if not isdead and not iswon:
 		touchingenemy = true
 
 func stop_damage() -> void:
 	touchingenemy = false
 
+func win() -> void:
+	if isdead or iswon:
+		return
+		
+	print("Win function called!")
+	iswon = true
+	wintimer = 4.0
+	velocity = Vector2.ZERO
+	touchingenemy = false
+	runsfx.stop()
+	
+	if winsfx:
+		winsfx.play()
+	
+	if winpanel and wintext:
+		print("Showing win message")
+		winpanel.visible = true
+		wintext.text = "[center][color=gold][wave][font_size=48]VICTORY![/font_size][/wave][/color][/center]\n\n[center][color=white][font_size=24]The warrior has returned to pay respects to his fallen king.[/font_size][/color][/center]\n\n[center][color=yellow][font_size=20][fade]Rest in peace, noble ruler...[/fade][/font_size][/color][/center]"
+	else:
+		print("ERROR: winpanel or wintext is null!")
+
 func checkcollect() -> void:
-	if isdead:
+	if isdead or iswon:
 		return
 		
 	var playertile = collectlayer.local_to_map(collectlayer.to_local(global_position))
